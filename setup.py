c50b379b194a7c831a74e3c91a450f86d1d8b098
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-15 16:03:17 +0800

    alvistack/2.1.0
        git clean -xdf
        tar zcvf ../python-iniconfig_2.1.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-iniconfig.spec ../python-iniconfig_2.1.0-1.spec
        cp ../python*-iniconfig*2.1.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/pytest-dev-iniconfig-2.1.0/
        rm -rf ../python*-iniconfig*2.1.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='iniconfig',
+    version='2.1.0',
+    description='brain-dead simple config-ini parsing',
+    author_email='Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>, Holger Krekel <holger.krekel@gmail.com>',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+    packages=[
+        'iniconfig',
+    ],
+    package_dir={'': 'src'},
+)
